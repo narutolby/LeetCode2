f61625024948f5ade8e6ad39c36dd2585d01582a
narutolby <willianlby@aliyun.com>
2015-06-21 16:17:03 +0800

    Rectange Area

--- src/main/java/com/lby/Rectangle_Area.java
+++ src/main/java/com/lby/Rectangle_Area.java
@@ -5,9 +5,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Assume that the total area is never beyond the maximum possible value of int.
+ * 
  * Find the total area covered by two rectilinear rectangles in a 2D plane.
  * Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+ * 
+ *                      Y                                        
+ *                      ^                                        
+ *                      |                                        
+ *                      |                                        
+ *                      |                                        
+ *                      |                                        
+ *                      |     (C,D):(3,4)                        
+ *            +------------------+                               
+ *            |         |        |                               
+ *            |         |        |                               
+ *            |         |        |               (G,H):(9,2)     
+ *            |         +----------------------------+           
+ *            |         |        |                   |           
+ *            |         |        |                   |           
+ *            |         |        |                   |           
+ *            +---------|--------+-------------------|---------> X
+ *      (A,B):(-3,0)    |                            |           
+ *                      +----------------------------+           
+ *                  (E,F):(0,-1)                                 
+ *      
+ *      
+ *      
  * Assume that the total area is never beyond the maximum possible value of int.
+ * 
+ * Credits:Special thanks to @mithmatt for adding this problem, creating the above image and all test cases.
+ *               
  * @author: mingren.lby
  * Date: 15-6-6
  * Time: 下午8:36
